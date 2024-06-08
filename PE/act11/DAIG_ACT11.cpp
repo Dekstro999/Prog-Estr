9b67449c241237f91fad71d6be9f158c77a09e2a
Dekstro999 <diego.galindo14@uabc.edu.mx>
2024-06-08 14:23:18 -0700

    Del

--- PE/act11/DAIG_ACT11.cpp
+++ PE/act11/DAIG_ACT11.cpp
@@ -8,7 +8,6 @@
 int menu();
 void opciones();
 void agregarAutomatico(Alumno alumnos[], int *numAlumnos);
-void agregarManual(Alumno alumnos[], int *numAlumnos);
 void eliminarRegistro(Alumno alumnos[], int *numAlumnos);
 void buscar(Alumno alumnos[], int numAlumnos);
 int buscarSecuencial(Alumno alumnos[], int numAlumnos, int matricula);
@@ -18,9 +17,8 @@ void ordenarBurbuja(Alumno alumnos[], int numAlumnos);
 void imprimir(Alumno alumnos[], int numAlumnos);
 void imprimir_pagina(Alumno alumnos[], int numAlumnos, int pagina_actual);
 void guardarEnArchivoTXT(Alumno alumnos[], int numAlumnos);
-void guardarRegistrosDAT(Alumno alumnos[], int numAlumnos);
-void guardarRegistros(Alumno alumnos[], int numAlumnos);
-void imprimirRegistrosDAT();
+
+
 
 short int orden;
 
@@ -40,15 +38,14 @@ int menu()
   cls();
   printf("\tMENU\n");
   printf("1.- AGREGAR (AUTOM %d REGISTROS)\n", AUTOMATICOS);
-  printf("2.- AGREGAR MANUAL\n");
-  printf("3.- ELIMINAR REGISTRO (logico)\n");
-  printf("4.- BUSCAR\n");
-  printf("5.- ORDENAR\n");
-  printf("6.- IMPRIMIR\n");
-  printf("7.- ARCHIVO TXT\n");
+  printf("2.- ELIMINAR REGISTRO (logico)\n");
+  printf("3.- BUSCAR\n");
+  printf("4.- ORDENAR\n");
+  printf("5.- IMPRIMIR\n");
+  printf("6.- ARCHIVO TXT\n");
   printf("0.- SALIR\n");
   printf("ESCOGE UNA OPCION: ");
-  opcion = validar_num(0, 7);
+  opcion = validar_num(0, 6);
 
   return opcion;
 }
@@ -73,32 +70,24 @@ void opciones()
     {
     case 1:
       agregarAutomatico(alumnos, &numAlumnos);
-      // guardarRegistros(alumnos, numAlumnos);
 
       break;
     case 2:
-      printf(" \" INCATIVO TEMPORALMENTE \" ");
-      Sleep(2000);
-      // agregarManual(alumnos, &numAlumnos);
-      // guardarRegistros(alumnos, numAlumnos);
-
-      break;
-    case 3:
       imprimir(alumnos, numAlumnos);
       eliminarRegistro(alumnos, &numAlumnos);
       break;
-    case 4:
+    case 3:
       buscar(alumnos, numAlumnos);
       break;
-    case 5:
+    case 4:
       ordenarPorMatricula(alumnos, numAlumnos);
       break;
-    case 6:
+    case 5:
       imprimir(alumnos, numAlumnos);
       pause();
 
       break;
-    case 7:
+    case 6:
       guardarEnArchivoTXT(alumnos, numAlumnos);
       pause();
 
@@ -154,8 +143,6 @@ void agregarAutomatico(Alumno alumnos[], int *numAlumnos)
     int anioAleatorio, mesAleatorio, diaAleatorio;
     generarFechaAleatoria(&anioAleatorio, &mesAleatorio, &diaAleatorio);
 
-    // Asignar la fecha al alumno
-    // Asignar la fecha al alumno
     alumnos[*numAlumnos + i].fecha.anio = anioAleatorio;
     alumnos[*numAlumnos + i].fecha.mes = mesAleatorio;
     alumnos[*numAlumnos + i].fecha.dia = diaAleatorio;
@@ -172,18 +159,9 @@ void agregarAutomatico(Alumno alumnos[], int *numAlumnos)
     }
     asignarEstadoAleatorio(&alumnos[*numAlumnos + i]);
 
-    // printf("\n%d\n", alumnos[*numAlumnos + i].matricula); //mensaje de depuracion
-    // printf("%s\n", alumnos[*numAlumnos + i].nombre.nombre); //mensaje de depuracion
-    // printf("%s\n", alumnos[*numAlumnos + i].nombre.apPaterno);  //mensaje de depuracion
-    // printf("%s\n", alumnos[*numAlumnos + i].nombre.apMaterno);  //mensaje de depuracion
-    // printf("%d\n", alumnos[*numAlumnos + i].sexo);  //mensaje de depuracion
-    // printf("                 curp\n");
-
     generarCURP(alumnos[*numAlumnos + i].curp, alumnos[*numAlumnos + i].nombre.apPaterno,
                 alumnos[*numAlumnos + i].nombre.apMaterno, alumnos[*numAlumnos + i].nombre.nombre,
                 anioAleatorio, mesAleatorio, diaAleatorio, alumnos[*numAlumnos + i].sexo, alumnos[*numAlumnos + i].est);
-
-    // printf("%s\n", alumnos[*numAlumnos + i].curp); //mensaje de depuracion
   }
 
   *numAlumnos += AUTOMATICOS;
@@ -193,87 +171,6 @@ void agregarAutomatico(Alumno alumnos[], int *numAlumnos)
 }
 
 //*************************
-void agregarManual(Alumno alumnos[], int *numAlumnos)
-{
-  TextColor(DEF);
-
-  // cls(); // Verificar si hay espacio disponible para agregar mas alumnos
-  if (*numAlumnos >= MAX_ALUMNOS)
-  {
-    TextColor(ROJO);
-    printf("No se pueden agregar mas alumnos. El limite es de %d.\n", MAX_ALUMNOS);
-    TextColor(DEF);
-    pause();
-    return;
-  }
-
-  printf("Ingrese los datos del nuevo alumno:\n");
-
-  do
-  {
-    printf("Matricula: ");
-    alumnos[*numAlumnos].matricula = validarMatricula();
-    if (alumnos[*numAlumnos].matricula == -1)
-      continue; // Repetir si no es valido
-
-  } while (alumnos[*numAlumnos].matricula == -1);
-
-  char paterno[40];
-
-  do
-  {
-    printf("Apellido Paterno: ");
-    fgets(paterno, sizeof(paterno), stdin);
-    convertirMayusculas(paterno);
-
-  } while (!validar_letras(paterno));
-  enter(paterno, alumnos, numAlumnos, "apPaterno");
-
-  char materno[40];
-  do
-  {
-    printf("Apellido Materno: ");
-    fgets(materno, sizeof(materno), stdin);
-    convertirMayusculas(materno);
-
-  } while (!validar_letras(materno));
-  enter(materno, alumnos, numAlumnos, "apMaterno");
-
-  char nombre[40];
-  do
-  {
-    printf("Nombre: ");
-    fgets(nombre, sizeof(nombre), stdin);
-    convertirMayusculas(nombre);
-
-  } while (!validar_letras(nombre));
-
-  enter(nombre, alumnos, numAlumnos, "nombre");
-  // do
-  // {
-  //   printf("Edad: ");
-  //   alumnos[*numAlumnos].edad = validar_num();
-  //   if (alumnos[*numAlumnos].edad != -1 && (1 > alumnos[*numAlumnos].edad ||
-  //                                           alumnos[*numAlumnos].edad > 80))
-  //   {
-  //     printf("Entre 1- 80 \n");
-  //     pause();
-  //     borrarlinea(3);
-  //     alumnos[*numAlumnos].edad = -1;
-  //   }
-
-  // } while (alumnos[*numAlumnos].edad == -1);
-
-  alumnos[*numAlumnos].sexo = elegirSexo();
-
-  alumnos[*numAlumnos].status = 1; // Marcar el registro como activo
-  // Incrementar el contador de alumnos
-  (*numAlumnos)++;
-
-  printf("Registro agregado correctamente.\n");
-  orden = 0;
-  pause();
-}
 
 //****************************
 void eliminarRegistro(Alumno alumnos[], int *numAlumnos)
@@ -298,7 +195,6 @@ void eliminarRegistro(Alumno alumnos[], int *numAlumnos)
 
   if (indice != -1)
   {
-    // Marcar el registro como inactivo
     alumnos[indice].status = 0;
     printf("El alumno con matricula %d ha sido eliminado correctamente.\n", matricula);
   }
@@ -341,7 +237,7 @@ void buscar(Alumno alumnos[], int numAlumnos)
 
     if (indice != -1)
     {
-      if (alumnos[indice].status != 1) // Verificar si el registro esta activo
+      if (alumnos[indice].status != 1)
       {
         TextColor(ROJO);
         printf("****  La matricula %d esta inactiva.\n", matricula);
@@ -431,29 +327,7 @@ int buscarBinaria(Alumno alumnos[], int numAlumnos, int matricula)
 void ordenarPorMatricula(Alumno alumnos[], int numAlumnos)
 {
   TextColor(DEF);
-
-  int opcion;
-  cls();
-  printf("\nMetodos de Ordenacion:\n");
-  printf("1. Burbuja\n");
-  printf("2. Otro Metodo (a implementar)\n");
-  printf("0. Volver al menu principal\n");
-  printf("Seleccione un metodo de ordenacion: ");
-  opcion = validar_num(0, 2);
-  switch (opcion)
-  {
-  case 0:
-    printf("\nVolviendo al menu principal...\n");
-    Sleep(1000);
-    break;
-  case 1:
-    ordenarBurbuja(alumnos, numAlumnos);
-    break;
-  case 2:
-    printf("\nPROXIMOS.\n");
-    pause();
-    break;
-  }
+  ordenarBurbuja(alumnos, numAlumnos);
 }
 
 void ordenarBurbuja(Alumno alumnos[], int numAlumnos)
@@ -499,7 +373,7 @@ void imprimir(Alumno alumnos[], int numAlumnos)
       pagina_actual++;
     }
   } while (opcion == 'S' || opcion == 's');
-  imprimirRegistrosDAT();
+  // imprimirRegistrosDAT();
 }
 
 void imprimir_pagina(Alumno alumnos[], int numAlumnos, int pagina_actual)
@@ -541,15 +415,13 @@ void imprimir_pagina(Alumno alumnos[], int numAlumnos, int pagina_actual)
 void guardarEnArchivoTXT(Alumno alumnos[], int numAlumnos)
 {
   FILE *archivo;
-  archivo = fopen("alumnos.txt", "w"); // Abrir el archivo en modo de escritura
+  archivo = fopen("BASURITA.txt", "w"); // Abrir el archivo en modo de escritura
 
   if (archivo == NULL)
   {
     printf("Error al abrir el archivo.");
     return;
   }
-
-  // Escribir los datos de los alumnos en el archivo
   fprintf(archivo, "LISTA DE ALUMNOS\n");
   fprintf(archivo, "=================================================================================================================================\n");
   fprintf(archivo, "| No.\t| Matricula | Apellido Paterno | Apellido Materno | Nombre\t\t\t\t| Fecha      | Edad | Sexo | CURP\t\t\t\t|\n");
@@ -578,78 +450,4 @@ void guardarEnArchivoTXT(Alumno alumnos[], int numAlumnos)
   fprintf(archivo, "=================================================================================================================================\n\n");
 
   fclose(archivo); // Cerrar el archivo
-}
-
-void guardarRegistrosDAT(Alumno alumnos[], int numAlumnos)
-{
-  FILE *archivo;
-  archivo = fopen("registros.dat", "ab");
-  if (archivo == NULL)
-  {
-    printf("Error al abrir el archivo.\n");
-    pause();
-    return;
-  }
-
-  // Alumno alumnoExistente;
-  // while (fread(&alumnoExistente, sizeof(Alumno), 1, archivo) == 1)
-  // {
-  //   // por si quisiera mostrar lo guardado
-  // }
-
-  // Escribir el nuevo al final del anterior
-  fwrite(alumnos, sizeof(Alumno), numAlumnos, archivo);
-
-  fclose(archivo);
-}
-
-void guardarRegistros(Alumno alumnos[], int numAlumnos)
-{
-  guardarRegistrosDAT(alumnos, numAlumnos);
-}
-
-void imprimirRegistrosDAT()
-{
-  FILE *archivo;
-  archivo = fopen("registros.dat", "rb");
-  if (archivo == NULL)
-  {
-    printf("Error al abrir el archivo.\n");
-    return;
-  }
-
-  printf("LISTA DE ALUMNOS (desde archivo DAT)\n");
-  printf("================================================================================================================\n");
-  printf("| No.\t| Matricula | Apellido Paterno | Apellido Materno | Nombre\t\t| Fecha      | Edad | Sexo | CURP          |\n");
-  printf("================================================================================================================\n");
-
-  int contador = 0;
-  Alumno alumno;
-
-  while (fread(&alumno, sizeof(Alumno), 1, archivo) == 1)
-  {
-    char sexo;
-    if (alumno.sexo == 1)
-    {
-      sexo = 'H'; // H para hombre
-    }
-    else
-    {
-      if (alumno.sexo == 2)
-      {
-        sexo = 'M'; // M para mujer
-      }
-    }
-    printf("| %4d\t| %-9d | %-16s | %-16s | %-17s\t| %02d/%02d/%4d | %-3d  | %-2c   | %-18s |\n",
-           contador + 1, alumno.matricula, alumno.nombre.apPaterno, alumno.nombre.apMaterno,
-           alumno.nombre.nombre, alumno.fecha.dia, alumno.fecha.mes, alumno.fecha.anio, alumno.edad, sexo, alumno.curp);
-
-    contador++;
-  }
-
-  printf("================================================================================================================\n");
-  printf("| Total de alumnos: %-80d\t\t\b|\n", contador);
-  printf("================================================================================================================\n\n");
-
-  fclose(archivo);
 }
